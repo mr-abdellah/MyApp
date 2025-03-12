e65c76d65dab84de448e065d2e2557de92e2076b
mr-abdellah <belkaid.abdulah@gmail.com>
2025-03-12 15:37:20 +0100

    Add Home and Payment Success screens with navigation setup

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {GPay} from 'react-native-epay-guiddini-rn-sdk';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => (
+  <GPay
+    secret_key="SEC-9ry6C5CZJLHoFyxQ3my0a8CainK1G7Li"
+    app_key="APP-DAGXCCFYE3AOA8H5DF">
+    <App />
+  </GPay>
+);
+
+AppRegistry.registerComponent(appName, () => Root);
