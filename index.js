75766f16e10d62d9c812ac5daeb245f2d80fb7ac
mr-abdellah <belkaid.abdulah@gmail.com>
2025-03-15 10:42:45 +0100

    Update GPay integration to use GPayProvider and upgrade react-native-epay-guiddini-rn-sdk to version 0.2.0

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {GPay} from 'react-native-epay-guiddini-rn-sdk';
+import {GPayProvider} from 'react-native-epay-guiddini-rn-sdk';
 
 const Root = () => (
-  <GPay
+  <GPayProvider
     secret_key="SEC-9ry6C5CZJLHoFyxQ3my0a8CainK1G7Li"
     app_key="APP-DAGXCCFYE3AOA8H5DF">
     <App />
-  </GPay>
+  </GPayProvider>
 );
 
 AppRegistry.registerComponent(appName, () => Root);
